c0ce8870d224238aab7f3e37cad15ffd3cade67f
Federico Devigili <f.devigili@alpitronic.it>
2022-08-23 14:46:10 +0200

    add framebuffer backend

---
+++ example/linux_framebuffer/linux_framebuffer_backend.cpp
@@ -0,0 +1,61 @@
+#include "linux_framebuffer_backend.h"
+
+#include "../render/depth.h"
+#include "../render/pixel.h"
+#include "../render/renderer.h"
+#include "../render/texture.h"
+
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <sys/mman.h> 
+#include <sys/ioctl.h> 
+#include <fcntl.h> 
+#include <linux/fb.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+
+Vec4i rect;
+Vec2i totalSize;
+
+PingoDepth * zetaBuffer;
+Pixel * frameBuffer;
+
+bool initFlag = false;
+
+void init( Renderer * ren, BackEnd * backEnd, Vec4i _rect) {
+    rect = _rect;
+
+}
+
+void beforeRender( Renderer * ren, BackEnd * backEnd) {
+    LinuxFramebufferBackEnd * this = (LinuxFramebufferBackEnd *) backEnd;
+}
+
+void afterRender( Renderer * ren,  BackEnd * backEnd) {
+
+}
+
+Pixel * getFrameBuffer( Renderer * ren,  BackEnd * backEnd) {
+    return frameBuffer;
+}
+
+PingoDepth * getZetaBuffer( Renderer * ren,  BackEnd * backEnd) {
+    return zetaBuffer;
+}
+
+
+
+void linuxFramebufferBackEndInit( LinuxFramebufferBackEnd * this, Vec2i size) {
+    totalSize = size;
+    this->backend.init = &init;
+    this->backend.beforeRender = &beforeRender;
+    this->backend.afterRender = &afterRender;
+    this->backend.getFrameBuffer = &getFrameBuffer;
+    this->backend.getZetaBuffer = &getZetaBuffer;
+    this->backend.drawPixel = 0;
+
+    zetaBuffer = malloc(size.x*size.y*sizeof (PingoDepth));
+    int fdScreen = open( "/dev/fb0", O_RDWR );
+    frameBuffer = mmap( 0, 1376*768*4, PROT_READ | PROT_WRITE, MAP_SHARED, fdScreen, 0 );    
+}
+
---
+++ example/linux_framebuffer/linux_framebuffer_backend.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include "../render/backend.h"
+#include "../math/vec2.h"
+
+typedef struct Pixel Pixel;
+
+typedef  struct {
+    BackEnd backend;
+    Vec2i size;
+} LinuxFramebufferBackEnd;
+
+
+void linuxFramebufferBackEndInit(LinuxFramebufferBackEnd * this, Vec2i size);
+
