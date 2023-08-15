d6ed920d310e565ee328611dcfd914c566d30bef
Federico Devigili <f.devigili@alpitronic.it>
2023-08-15 20:38:28 +0200

    Fix setup of viking rendering, rotate for x11 backend

--- example/linux_window/main.c
+++ example/linux_window/main.c
@@ -1,20 +1,19 @@
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include "math/mat4.h"
+#include "assets/viking.h"
+#include "linux_window_backend.h"
+
+#include "render/entity.h"
 #include "render/material.h"
 #include "render/mesh.h"
 #include "render/object.h"
 #include "render/pixel.h"
 #include "render/renderer.h"
-#include "render/entity.h"
 
-#include "linux_window_backend.h"
-
-#include "assets/viking.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
 Pixel * loadTexture(char * filename, Vec2i size) {
@@ -60,22 +59,23 @@ int main(){
     linuxWindowBackendInit(&backend, size);
 
     Renderer renderer;
-    renderer_init(&renderer, size, &backend );
+    renderer_init(&renderer, size, (Backend*)&backend );
     renderer_set_root_renderable(&renderer, (Renderable*)&root_entity);
 
     float phi = 0;
     Mat4 t;
 
-    renderer.camera_projection = mat4Perspective( 1, 2500.0,(float)size.x / (float)size.y, 0.6);
+    renderer.camera_projection = mat4Perspective(1, 500.0, (float) size.x / (float) size.y, 1);
 
-    Mat4 translate_back = mat4Translate((Vec3f) { 0,0,-35});
-    Mat4 rotate_down = mat4RotateX(0.40);
-    renderer.camera_view = mat4MultiplyM(&rotate_down, &translate_back );
+    Mat4 translate_back = mat4Translate((Vec3f){0, 0, -35});
+    Mat4 rotate_down = mat4RotateX(-0.30);
+    renderer.camera_view = mat4MultiplyM(&rotate_down, &translate_back);
 
     while (1) {
+        Mat4 rotate1 = mat4RotateY(phi);
+        Mat4 rotate2 = mat4RotateX(3.1421);
+        root_entity.transform = mat4MultiplyM( &rotate1, &rotate2 );
 
-        //SCENE
-        root_entity.transform = mat4RotateY(phi);
         phi += 0.01;
 
         renderer_render(&renderer);
